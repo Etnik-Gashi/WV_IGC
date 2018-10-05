de3cb62306e3552a52a309f8098d4e14436f8d95
Etnik A. Gashi <37635686+Etnik-Gashi@users.noreply.github.com>
2018-10-06 00:13:50 +0200

    RegEx added
    Handling with GET and POST request, and input with regex.

--- server.go
+++ server.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"regexp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
+	switch r.Method {
+	case http.MethodGet:
+		temp,err:=template.ParseFiles("wv.html")
+		if err !=nil{
+			http.Error(w,"Error",http.StatusInternalServerError)
+		}
+		temp.Execute(w,nil)
+		break
+	case http.MethodPost:
+		input:=r.PostFormValue("inputi")
+		pattern:=".*.igc"
+		res,err:=regexp.MatchString(pattern,input)
+		if err != nil{
+			http.Error(w,err.Error(),http.StatusInternalServerError)
+		}
+		if res{
+			fmt.Fprintln(w,"File format is correct")
+			return
+		}else {
+			fmt.Println("Invalid file format, only IGC file!!")
+		}
+		break
+	default:
+		http.Error(w,"Not implemented",http.StatusNotImplemented)
 
-	fmt.Fprintf(w, "<h1>Hi, here are igc info</h1>")
+
+	}
+
+
 }
 
 func main() {
