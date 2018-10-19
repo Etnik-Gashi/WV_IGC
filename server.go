05b4b3bad13a37ea7f14a0c567fe4c0c6d81c57f
Etnik-Gashi <etnik.gashi1@outlook.com>
2018-10-20 01:19:21 +0200

    Chechking metalinter suggestions

--- server.go
+++ server.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/marni/goigc"
 	"log"
+	"math/rand"
 	"os"
+
 	//"html/template"
 	"net/http"
 	"regexp"
-	"time"
-	igc "github.com/marni/goigc"
-	"math/rand"
-	"encoding/json"
 	"strconv"
+	"time"
 	//"path/filepath"
 	"strings"
 )
@@ -43,6 +45,7 @@ type Track struct {
 	ID string   `json:"id"`
 	IgcTrack igc.Track `json:"igc_track"`
 }
+
 //Attributes : the info about each igc file via id
 type Attributes struct{
 	HeaderDate string `json:"h_date"`
@@ -51,6 +54,7 @@ type Attributes struct{
 	GliderID string 	`json:"glider_id"`
 	Length float64 `json:"track_length"`
 }
+
 //Calculating uptime based on ISO 8601
 func timeSince(t time.Time) string {
 
@@ -77,184 +81,175 @@ func timeSince(t time.Time) string {
 
 //Handling based in parsing url
 func handler(w http.ResponseWriter,r *http.Request) {
+
+	//Handling for /igcinfo and for /<rubbish>
+
+	http.Error(w, "404 - Page not found!", http.StatusNotFound)
+
+}
+func handler1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	parts := strings.Split(r.URL.Path, "/")
 	//var empty = regexp.MustCompile(``)
 	var api= regexp.MustCompile(`api`)
-	switch {
-	//Handling for /igcinfo and for /<rubbish>
-	case len(parts) == 2 :
-		http.Error(w, "404 - Page not found!", http.StatusNotFound)
-		break
 
-		//Handling for /igcinfo/api
-	case len(parts) == 3 && api.MatchString(parts[2]) :
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, "{"+"\"uptime\": \""+timeSince(timeStarted)+"\","+"\"info\": \"Service for IGC tracks.\","+"\"version\": \"v1\""+"}")
-		break
-		//Handling for /igcinfo/api/igc
-	case len(parts) == 4 :
-		{
-
-			w.Header().Set("Content-Type", "application/json")
-			var rNum= regexp.MustCompile(`/igcinfo/api/igc`)
-
-			switch {
-
-			case rNum.MatchString(r.URL.Path):
-
-				switch r.Method {
-				//Handling GET /igcinfo/api/igc for returning all ids storing in a slice
-				case http.MethodGet:
-					ids := make([]string, 0, 0)
-
-					for i := range igcFiles {
-						ids = append(ids, igcFiles[i].ID)
-					}
-
-					json.NewEncoder(w).Encode(ids)
-
-					break
-				case http.MethodPost:
-
-					//handling post /igcinfo/api/igc for sending a url and returning an id for that url
-					pattern := ".*.igc"
-
-					URL := &_url{}
-
-					var error= json.NewDecoder(r.Body).Decode(URL)
-					if error != nil {
-						fmt.Fprintln(w, "Error!! ", error)
-						return
-					}
-					res, err := regexp.MatchString(pattern, URL.URL)
-					if err != nil {
-						http.Error(w, err.Error(), http.StatusInternalServerError)
-						fmt.Fprintln(w, "Error!! ", error)
-						return
-					}
-					if res {
-
-						track, _ := igc.ParseLocation(URL.URL)
-
-						ID := rand.Intn(1000)
-
-						igcFile := Track{}
-						igcFile.ID = strconv.Itoa(ID)
-						igcFile.IgcTrack = track
-
-						igcFiles = append(igcFiles, igcFile)
-
-						json.NewEncoder(w).Encode(igcFile.ID)
-						return
-					}
-					break
-				default:
-					http.Error(w, "Not implemented", http.StatusNotImplemented)
-
-				}
-			default:
-				http.Error(w, "404 - Page not found!", http.StatusNotFound)
-				break
-			}
-
-		}
-		break
-	case len(parts) == 5 : {
-		//Handling /igcinfo/api/igc/<id>
-
-		w.Header().Set("Content-Type", "application/json")
-
-		attributes := &Attributes{}
-
-		var rNum= regexp.MustCompile(`/igcinfo/api/igc/\d{1,}`)
-
-		switch {
-		case rNum.MatchString(r.URL.Path):
-
-			for i := range igcFiles {
-
-				if igcFiles[i].ID == parts[4] {
-					attributes.HeaderDate = igcFiles[i].IgcTrack.Header.Date.String()
-					attributes.Pilot = igcFiles[i].IgcTrack.Pilot
-					attributes.Glider = igcFiles[i].IgcTrack.GliderType
-					attributes.GliderID = igcFiles[i].IgcTrack.GliderID
-					attributes.Length = trackLength(igcFiles[i].IgcTrack)
-
-					json.NewEncoder(w).Encode(attributes)
-				}
-				//Handling if user type different id from ids stored
-				if igcFiles[i].ID != parts[4]{
-					http.Error(w,"", http.StatusNotFound)
-				}
-
-			}
-
-			break
-		default:
-			http.Error(w, "400 - Bad Request, the field you entered is not on our database!", http.StatusBadRequest)
-
-		}
+	//Handling for /igcinfo/api
+	if len(parts) != 3 || !api.MatchString(parts[2]) {
+		http.Error(w, "400 - Bad Request, too many url arguments.", http.StatusBadRequest)
+		return
 	}
-		break
+	fmt.Fprintln(w, "{"+"\"uptime\": \""+timeSince(timeStarted)+"\","+"\"info\": \"Service for IGC tracks.\","+"\"version\": \"v1\""+"}")
 
-		//Handling for GET /api/igc/<id>/<field>
-	case len(parts) == 6 :{
-		var rNum= regexp.MustCompile(`/igcinfo/api/igc/\d{1,}/\w{1,}`)
+}
+//Handling for /igcinfo/api/igc
+func handler2(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
 
-		switch {
 
-		case rNum.MatchString(r.URL.Path):
+	switch r.Method {
+	//Handling GET /igcinfo/api/igc for returning all ids storing in a slice
+	case http.MethodGet:
+		ids := make([]string, 0)
 
-			for i := range igcFiles {
-
-				if igcFiles[i].ID == parts[4] {
-					switch {
-					case parts[5] == "pilot":
-						fmt.Fprintln(w,igcFiles[i].IgcTrack.Pilot)
-						break
-					case parts[5] == "glider":
-						fmt.Fprintln(w,igcFiles[i].IgcTrack.GliderType)
-						break
-					case parts[5] == "glider_id":
-						fmt.Fprintln(w,igcFiles[i].IgcTrack.GliderID)
-						break
-					case parts[5] == "track_length":
-						fmt.Fprintln(w,trackLength(igcFiles[i].IgcTrack))
-						break
-					case parts[5] == "h_date":
-						fmt.Fprintln(w,igcFiles[i].IgcTrack.Header.Date.String())
-						break
-					default:
-						http.Error(w, "400 - Bad Request, the field you entered is not on our database!", http.StatusBadRequest)
-						break
-					}
-
-				}
-
-			}
-			break
-		default:
-			http.Error(w, "400 - Bad Request", http.StatusBadRequest)
-
+		for i := range igcFiles {
+			ids = append(ids, igcFiles[i].ID)
 		}
 
-	}
-		break
-	case len(parts)>6:{
-		http.Error(w, "404 - Page not found!", http.StatusNotFound)
+		json.NewEncoder(w).Encode(ids)
 
-	}
-		break
+
+	case http.MethodPost:
+
+		//handling post /igcinfo/api/igc for sending a url and returning an id for that url
+		pattern := ".*.igc"
+
+		URL := &_url{}
+
+		var error = json.NewDecoder(r.Body).Decode(URL)
+		if error != nil {
+			fmt.Fprintln(w, "Error!! ", error)
+			return
+		}
+		res, err := regexp.MatchString(pattern, URL.URL)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if res {
+
+			track, _ := igc.ParseLocation(URL.URL)
+
+			ID := rand.Intn(1000)
+
+			igcFile := Track{}
+			igcFile.ID = strconv.Itoa(ID)
+			igcFile.IgcTrack = track
+
+			igcFiles = append(igcFiles, igcFile)
+
+			json.NewEncoder(w).Encode(igcFile.ID)
+			return
+		}
 
 	default:
-		http.Error(w, "404 - Page not found!", http.StatusNotFound)
-		break
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
+		return
+	}
+
+}
+func handler3(w http.ResponseWriter, r *http.Request){
+	//Handling /igcinfo/api/igc/<id>
+
+	w.Header().Set("Content-Type", "application/json")
+	idURL := mux.Vars(r)
+
+
+	rNum, _ := regexp.Compile(`[0-9]+`)
+	if !rNum.MatchString(idURL["id"]) {
+		http.Error(w, "400 - Bad Request", http.StatusBadRequest)
+		return
+	}
+
+
+	attributes := &Attributes{}
+
+	for i := range igcFiles {
+
+		if igcFiles[i].ID == idURL["id"] {
+			attributes.HeaderDate = igcFiles[i].IgcTrack.Header.Date.String()
+			attributes.Pilot = igcFiles[i].IgcTrack.Pilot
+			attributes.Glider = igcFiles[i].IgcTrack.GliderType
+			attributes.GliderID = igcFiles[i].IgcTrack.GliderID
+			attributes.Length = trackLength(igcFiles[i].IgcTrack)
+
+			json.NewEncoder(w).Encode(attributes)
+		}
+		//Handling if user type different id from ids stored
+
+
+	}
+	http.Error(w, "404 - The trackInfo with that id doesn't exists in IGC Files", http.StatusNotFound)
+
+
+
+}
+
+
+func handler4(w http.ResponseWriter,r *http.Request) {
+
+	//Handling for GET /api/igc/<id>/<field>
+	w.Header().Set("Content-Type", "application/json")
+
+	urlFields := mux.Vars(r)
+
+	var rNum, _ = regexp.Compile(`[a-zA-Z_]+`)
+
+	if !rNum.MatchString(urlFields["field"]) {
+		http.Error(w, "400 - Bad Request, wrong parameters", http.StatusBadRequest)
+		return
+	}
+
+	for i := range igcFiles {
+
+		if igcFiles[i].ID == urlFields["id"] {
+			switch {
+			case urlFields["field"] == "pilot":
+				json.NewEncoder(w).Encode(igcFiles[i].IgcTrack.Pilot)
+			case urlFields["field"] == "glider":
+				json.NewEncoder(w).Encode(igcFiles[i].IgcTrack.GliderType)
+
+			case urlFields["field"] == "glider_id":
+				json.NewEncoder(w).Encode(igcFiles[i].IgcTrack.GliderID)
+
+			case urlFields["field"] == "track_length":
+				json.NewEncoder(w).Encode( trackLength(igcFiles[i].IgcTrack))
+
+			case urlFields["field"] == "h_date":
+				json.NewEncoder(w).Encode( igcFiles[i].IgcTrack.Header.Date.String())
+
+			default:
+				http.Error(w, "400 - Bad Request, the field you entered is not on our database!", http.StatusBadRequest)
+				return
+			}
+
+		}else {
+			http.Error(w, "400 - Bad Request, the field you entered is not on our database!", http.StatusBadRequest)
+			return
+		}
+
 	}
 }
 
-func main() {
 
-	http.HandleFunc("/",handler)
+func main() {
+	r:=mux.NewRouter()
+	r.HandleFunc("/igcinfo",handler)
+	r.HandleFunc("/igcinfo/api",handler1)
+	r.HandleFunc("/igcinfo/api/igc",handler2)
+	r.HandleFunc("/igcinfo/api/igc/{id}",handler3)
+	r.HandleFunc("/igcinfo/api/igc/{id}/{field}",handler4)
+
 
 	//fmt.Println("listening...")
 
